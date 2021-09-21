14ed91c6c801609cfcb317387eee5635efc0b4c2
Anderson Santos de Farias <andersonsantofarias@outlook.com>
2021-09-21 17:58:44 -0300

    feat: Function declaration.

---
+++ foundation/anatomy-1.js
@@ -0,0 +1,24 @@
+// Function declaration
+
+function sayHello() {
+    console.log('Hello!')
+}
+
+function sayHelloTo(name) {
+    console.log(`Hello ${name}`)
+}
+
+sayHello()
+sayHelloTo('Anderson')
+
+function returnHi() {
+    return 'Hi'
+}
+
+console.log(returnHi())
+
+function returnHiTo(name) {
+    return `Hi ${name}`
+}
+
+console.log(returnHiTo('Anderson'))
