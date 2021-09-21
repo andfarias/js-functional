eee0e5c3138f2854430b99439af360e09aa57038
Anderson Santos de Farias <andersonsantofarias@outlook.com>
2021-09-21 18:02:54 -0300

    feat: Challenge function.

---
+++ foundation/challenge.js
@@ -0,0 +1,16 @@
+// create a range function
+function range(a, b, s) {
+    const n1 = b === undefined ? 1 : a
+    const n2 = b === undefined ? a : b
+    const step = n1 <= n2 ? Math.abs(s) : -Math.abs(s)
+
+    const nums = []
+    for (let i = n1; n1 <= n2 ? i <= n2 : i >= n2; i += step) {
+        nums.push(i)
+    }
+    return nums
+}
+console.log(range(5))
+console.log(range(6, 11))
+console.log(range(10, 19, 2))
+console.log(range(6, 2))
