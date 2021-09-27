49696fb04d885f8f4c51eb507768fabddb3a0042
Anderson Santos de Farias <andersonsantofarias@outlook.com>
2021-09-27 17:57:21 -0300

    feat: Implement concept promise (resolve, reject, All).

---
+++ functions/promise_4.js
@@ -0,0 +1,27 @@
+function gerarNumerosEntre(min, max, tempo) {
+    if (min > max) [max, min] = [min, max]
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const fator = max - min + 1
+            const aleatorio = parseInt(Math.random() * fator) + min
+            resolve(aleatorio)
+        }, tempo)
+    });
+}
+
+function gerarVariosNumeros() {
+    return Promise.all([
+        gerarNumerosEntre(1, 60, 4000),
+        gerarNumerosEntre(1, 60, 225),
+        gerarNumerosEntre(1, 60, 555),
+        gerarNumerosEntre(1, 60, 1000),
+        gerarNumerosEntre(1, 60, 2345),
+    ])
+}
+
+console.time('promise')
+gerarVariosNumeros()
+    .then(console.log)
+    .then(() => {
+        console.timeEnd('promise')
+    })
